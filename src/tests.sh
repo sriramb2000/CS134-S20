314fef49f47be848886f915f24c3927bcccec8a2
Sriram Balachandran <sriramb2000@gmail.com>
2020-06-01 12:14:23 -0700

    test: add testing script

---
+++ src/tests.sh
@@ -0,0 +1,203 @@
+rm results
+rm -rf tmp
+mkdir tmp
+echo test 1
+go test > tmp/result1
+echo test 1 >> results
+tail -3 tmp/result1 >> results
+echo test 2
+go test > tmp/result2
+echo test 2 >> results
+tail -3 tmp/result2 >> results
+echo test 3
+go test > tmp/result3
+echo test 3 >> results
+tail -3 tmp/result3 >> results
+echo test 4
+go test > tmp/result4
+echo test 4 >> results
+tail -3 tmp/result4 >> results
+echo test 5
+go test > tmp/result5
+echo test 5 >> results
+tail -3 tmp/result5 >> results
+echo test 6
+go test > tmp/result6
+echo test 6 >> results
+tail -3 tmp/result6 >> results
+echo test 7
+go test > tmp/result7
+echo test 7 >> results
+tail -3 tmp/result7 >> results
+echo test 8
+go test > tmp/result8
+echo test 8 >> results
+tail -3 tmp/result8 >> results
+echo test 9
+go test > tmp/result9
+echo test 9 >> results
+tail -3 tmp/result9 >> results
+echo test 10
+go test > tmp/result10
+echo test 10 >> results
+tail -3 tmp/result10 >> results
+echo test 11
+go test > tmp/result11
+echo test 11 >> results
+tail -3 tmp/result11 >> results
+echo test 12
+go test > tmp/result12
+echo test 12 >> results
+tail -3 tmp/result12 >> results
+echo test 13
+go test > tmp/result13
+echo test 13 >> results
+tail -3 tmp/result13 >> results
+echo test 14
+go test > tmp/result14
+echo test 14 >> results
+tail -3 tmp/result14 >> results
+echo test 15
+go test > tmp/result15
+echo test 15 >> results
+tail -3 tmp/result15 >> results
+echo test 16
+go test > tmp/result16
+echo test 16 >> results
+tail -3 tmp/result16 >> results
+echo test 17
+go test > tmp/result17
+echo test 17 >> results
+tail -3 tmp/result17 >> results
+echo test 18
+go test > tmp/result18
+echo test 18 >> results
+tail -3 tmp/result18 >> results
+echo test 19
+go test > tmp/result19
+echo test 19 >> results
+tail -3 tmp/result19 >> results
+echo test 20
+go test > tmp/result20
+echo test 20 >> results
+tail -3 tmp/result20 >> results
+echo test 21
+go test > tmp/result21
+echo test 21 >> results
+tail -3 tmp/result21 >> results
+echo test 22
+go test > tmp/result22
+echo test 22 >> results
+tail -3 tmp/result22 >> results
+echo test 23
+go test > tmp/result23
+echo test 23 >> results
+tail -3 tmp/result23 >> results
+echo test 24
+go test > tmp/result24
+echo test 24 >> results
+tail -3 tmp/result24 >> results
+echo test 25
+go test > tmp/result25
+echo test 25 >> results
+tail -3 tmp/result25 >> results
+echo test 26
+go test > tmp/result26
+echo test 26 >> results
+tail -3 tmp/result26 >> results
+echo test 27
+go test > tmp/result27
+echo test 27 >> results
+tail -3 tmp/result27 >> results
+echo test 28
+go test > tmp/result28
+echo test 28 >> results
+tail -3 tmp/result28 >> results
+echo test 29
+go test > tmp/result29
+echo test 29 >> results
+tail -3 tmp/result29 >> results
+echo test 30
+go test > tmp/result30
+echo test 30 >> results
+tail -3 tmp/result30 >> results
+echo test 31
+go test > tmp/result31
+echo test 31 >> results
+tail -3 tmp/result31 >> results
+echo test 32
+go test > tmp/result32
+echo test 32 >> results
+tail -3 tmp/result32 >> results
+echo test 33
+go test > tmp/result33
+echo test 33 >> results
+tail -3 tmp/result33 >> results
+echo test 34
+go test > tmp/result34
+echo test 34 >> results
+tail -3 tmp/result34 >> results
+echo test 35
+go test > tmp/result35
+echo test 35 >> results
+tail -3 tmp/result35 >> results
+echo test 36
+go test > tmp/result36
+echo test 36 >> results
+tail -3 tmp/result36 >> results
+echo test 37
+go test > tmp/result37
+echo test 37 >> results
+tail -3 tmp/result37 >> results
+echo test 38
+go test > tmp/result38
+echo test 38 >> results
+tail -3 tmp/result38 >> results
+echo test 39
+go test > tmp/result39
+echo test 39 >> results
+tail -3 tmp/result39 >> results
+echo test 40
+go test > tmp/result40
+echo test 40 >> results
+tail -3 tmp/result40 >> results
+echo test 41
+go test > tmp/result41
+echo test 41 >> results
+tail -3 tmp/result41 >> results
+echo test 42
+go test > tmp/result42
+echo test 42 >> results
+tail -3 tmp/result42 >> results
+echo test 43
+go test > tmp/result43
+echo test 43 >> results
+tail -3 tmp/result43 >> results
+echo test 44
+go test > tmp/result44
+echo test 44 >> results
+tail -3 tmp/result44 >> results
+echo test 45
+go test > tmp/result45
+echo test 45 >> results
+tail -3 tmp/result45 >> results
+echo test 46
+go test > tmp/result46
+echo test 46 >> results
+tail -3 tmp/result46 >> results
+echo test 47
+go test > tmp/result47
+echo test 47 >> results
+tail -3 tmp/result47 >> results
+echo test 48
+go test > tmp/result48
+echo test 48 >> results
+tail -3 tmp/result48 >> results
+echo test 49
+go test > tmp/result49
+echo test 49 >> results
+tail -3 tmp/result49 >> results
+echo test 50
+go test > tmp/result50
+echo test 50 >> results
+tail -3 tmp/result50 >> results
